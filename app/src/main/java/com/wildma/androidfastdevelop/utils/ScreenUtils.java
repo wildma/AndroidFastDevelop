6c553cabfb51b2505bfc147edecef5097ab8dbca
Alpha58 <maxionghui58@163.com>
2017-08-18 21:19:18 +0800

    add ScreenUtils

---
+++ app/src/main/java/com/wildma/androidfastdevelop/utils/ScreenUtils.java
@@ -0,0 +1,32 @@
+package com.wildma.androidfastdevelop.utils;
+
+import android.content.Context;
+
+/**
+ * Author     wildma
+ * DATE       2017/8/18
+ * Des	      ${屏幕相关工具类}
+ */
+public class ScreenUtils {
+
+    /**
+     * 获取屏幕宽度（px）
+     *
+     * @param context
+     * @return
+     */
+    public static int getScreenWidth(Context context) {
+        return context.getResources().getDisplayMetrics().widthPixels;
+    }
+
+
+    /**
+     * 获取屏幕高度（px）
+     *
+     * @param context
+     * @return
+     */
+    public static int getScreenHeight(Context context) {
+        return context.getResources().getDisplayMetrics().heightPixels;
+    }
+}
