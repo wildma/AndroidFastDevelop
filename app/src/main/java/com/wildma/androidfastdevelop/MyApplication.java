eb83ceb0ee4399ff759cba646ed4347b325a9a41
wildma <wildma58@163.com>
2018-06-27 21:40:48 +0800

    1. add FileUtils  2. add PermissionUtils 3. 优化其他代码

--- app/src/main/java/com/wildma/androidfastdevelop/MyApplication.java
+++ app/src/main/java/com/wildma/androidfastdevelop/MyApplication.java
@@ -1,6 +1,7 @@
 package com.wildma.androidfastdevelop;
 
 import android.app.Application;
+import android.content.Context;
 
 import com.wildma.androidfastdevelop.utils.CrashHandler;
 
@@ -11,9 +12,15 @@ import com.wildma.androidfastdevelop.utils.CrashHandler;
  */
 public class MyApplication extends Application {
 
+    private static Context mContext;
+
     @Override
     public void onCreate() {
         super.onCreate();
         CrashHandler.getInstance().init(this);//初始化CrashHandler
+    }
+
+    public static Context getContext() {
+        return mContext;
     }
 }
